a7546ee93198b73ce0bd4486174afc54dae6d74d
Karsten Dambekalns <karsten@dambekalns.de>
2024-03-08 23:07:39 +0100

    [Milaner] Adjust login copy

--- js/register.js
+++ js/register.js
@@ -45,7 +45,7 @@ function openRegisterForm() {
 }
 
 function forgotPassword() {
-	alert('Funktion ist noch in Arbeit. Bitte bei Justin melden!');
+	alert('Funktion ist noch in Arbeit. Bitte bei Karsten melden!');
 }
 
 function updatePreview() {
@@ -53,7 +53,7 @@ function updatePreview() {
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
     var previewDiv = document.getElementById("avatar-preview");
 
-    var imageUrl = "img/a" + selectedValue + ".png"; 
+    var imageUrl = "img/a" + selectedValue + ".png";
 
     previewDiv.innerHTML = "<img src='" + imageUrl + "' alt='Avatar Preview'>";
   }
