64dc6f00449af045c2b4c16bf5a8585f36e6bef9
Karsten Dambekalns <karsten@dambekalns.de>
2024-03-08 22:46:50 +0100

    [Milaner] Read DB connection from env variables

--- src/Database.php
+++ src/Database.php
@@ -16,9 +16,9 @@ class Database
             $port = 3306;
         } else {
             $servername = "localhost";
-            $username = "-";
-            $password = "-";
-            $dbname = "-";
+            $username = $_SERVER['DATABASE_USER'];
+            $password = $_SERVER['DATABASE_PASSWORD'];
+            $dbname = $_SERVER['DATABASE_NAME'];
             $port = 3306;
         }
 
